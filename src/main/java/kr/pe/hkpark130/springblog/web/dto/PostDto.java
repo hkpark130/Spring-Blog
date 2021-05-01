c089e576cf026d6c81952584265ecf8626b0ed5c
hkpark130 <hkpark130@naver.com>
2021-05-01 11:28:35 +0900

    https://hkpark130.p-e.kr:8080/issues/21

--- src/main/java/kr/pe/hkpark130/springblog/web/dto/PostDto.java
+++ src/main/java/kr/pe/hkpark130/springblog/web/dto/PostDto.java
@@ -13,18 +13,21 @@ public class PostDto {
     private Long id;
     private String title;
     private String contents;
+    private String category;
     private LocalDateTime modifiedDate;
 
     @Builder
-    public PostDto(String title, String contents) {
+    public PostDto(String title, String contents, String category) {
         this.title = title;
         this.contents = contents;
+        this.category = category;
     }
 
     public PostDto(Posts entity){
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.contents = entity.getContents();
+        this.category = entity.getCategory();
         this.modifiedDate = entity.getModifiedDate();
     } // 인덱스 화면에 뿌려주기 위한 생성자(findAllDesc) or findById
 
@@ -32,6 +35,8 @@ public class PostDto {
         return Posts.builder()
                 .title(title)
                 .contents(contents)
+                .category(category)
                 .build();
     } //repository로 저장을 위해
+
 }
